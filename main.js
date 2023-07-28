770ebc59eeeb6dd5a78c3b8c62b924e35a312540
Nless1 <128965460+Nless1@users.noreply.github.com>
2023-07-28 23:50:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstname = 'Danil'
+const lastname = 'Lobkovskiy'
+const age = '24'
+const city = 'Orenburg'
+const number = '89999999999'
+console.log(firstname, lastname, age, 'y.o', city, number);
